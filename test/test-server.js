9f022c3d1eaa58940155af22b73a5a5d90f09e4c
nickcanals <nick.canals@yahoo.com>
2023-05-04 18:17:29 +0000

    Fixed errors

--- test/test-server.js
+++ test/test-server.js
@@ -27,7 +27,7 @@ describe('GET /', function () {
         if (err) {
           return done(err);
         }
-        chai.assert.isTrue(res.text.includes("<h1>Welcome to [TEAM NAME'S] application</h1>"));
+        chai.assert.isTrue(res.text.includes("<h1>Welcome to the coolest app around!</h1>"));
         return done();
       });
 
